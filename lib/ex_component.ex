fef84a5d7aec817274e99e50276813a97f045282
Mohamad <husseini.mel@gmail.com>
2020-04-14 22:17:08 -0300

    Refactoring

--- lib/ex_component.ex
+++ lib/ex_component.ex
@@ -2,7 +2,7 @@ defmodule ExComponent do
   @moduledoc """
   This lib provides a DSL for generating HTML components.
 
-      defcomp(:alert, variants: [:primary, :success], class: "list-group", default_tag: :div)
+      defcomp(:alert, type: {:content_tag, :div}, variants: [:primary, :success], class: "list-group")
 
       alert "Alert!"
       #=> <div class="alert">Alert!</div>      
@@ -11,13 +11,18 @@ defmodule ExComponent do
       #=> <div class="alert alert-primary">Alert!</div>      
 
       alert :success, "Alert!"
-      #=> <div class="alert alert-success">Alert!</div>      
+      #=> <div class="alert alert-success">Alert!</div>    
 
-  Notice that a `component/2` clause is generated for the base component, and
-  a `component/3` clause for each variant.
+      alert :success, "Alert!", class: "extra"
+      #=> <div class="alert alert-success extra">Alert!</div>    
 
   All generated function clauses accept a block, and a list of opts that
   is forwarded onto the HTML.
+
+      alert do
+        "Alert!"
+      end
+      #=> <div class="alert">Alert!</div>
 
       alert class: "extra" do
         "Alert!"
@@ -27,14 +32,6 @@ defmodule ExComponent do
       alert :primary do
         "Alert!"
       end
-      #=> <div class="alert alert-primary">Alert!</div>
-
-      alert :primary, class: "extra" do
-        "Alert!"
-      end
-      #=> <div class="alert alert-primary extra">Alert!</div>
-
-      alert :primary, "Alert!", class: "extra"
       #=> <div class="alert alert-primary extra">Alert!</div>
 
   See below for an explanation of the options.
@@ -43,31 +40,24 @@ defmodule ExComponent do
 
     * `:class` - the component's class name. This option is required.
 
-    * `:block` - when `true`, generates an additional block syntax for each function clause. When `:only`, generates only block syntax for each function clause. Use `false` to disable block generation. Defaults to `true`.
-
-    * `:default_tag` - the component's HTML tag. This option is required.
-
-    * `:delegate` - delegates rendering to the given function. Defaults to `&Phoenix.HTML.Tag.content_tag/3`.
-    
     * `:html_opts` - a list of opts to forward onto the HTML.
 
     * `:nest` - nests the component in the given option. This option can be a function that generates another component or an atom HTML tag name.
 
     * `:prepend` - prepends the given content to the component. Can be a function that generates another component or an atom of a self-closing HTML tag.
 
+    * `:append` - appends the given content to the component. Can be a function that generates another component or an atom of a self-closing HTML tag.
+
     * `:variants` - a list of component variants. Each variant generates a `component/3` function clause.
 
   The `:class` is the base class of the component and is used to build
   variant classes in the form `class="{class class-variant}"`.
 
-  The `:default_tag` sets the default tag to use when building the component. It can
-  be overriden by passing the `:tag` option during function calls.
-
   The `:variants` option adds a modifier class to the component and automatically
   generates `component/3` function clauses for each variant, where the variant is the
   first argument.
 
-      defcomp(:alert, class: "alert", default_tag: :div, variants: [:success])
+      defcomp(:alert, type: {:content_tag, :div}, class: "alert", variants: [:success])
 
       alert :success, class: "extra" do
         "Alert!"
@@ -77,52 +67,19 @@ defmodule ExComponent do
   For components that can have multiple variants, use `component/2` and
   pass a list to the `:variant` option.
 
-      defcomp(:list_group, class: "list-group", default_tag: :div, variants: [:flush, :horizontal])
+      defcomp(:list_group, type: {:content_tag, :ul}, class: "list-group", variants: [:flush, :horizontal])
 
       list_group variant: [:flush, :horizontal], class: "extra" do
         "..."
       end
       #=> <div class="list-group list-group-flush list-group-horizontal ">...</div>
 
-  ## Blocks
+  ## Appending And Prepending Content
 
-  The `:block` option defaults to `true` and generates additional block syntax clauses for each function.
+  Use the `:append` and `:prepend` option to prepend or append other components to the component's
+  content. For example, an an alert that has a close button.
 
-      defcomp(:alert, block: true, ...)
-
-      alert "Alert!"
-      #=> <div class="alert">Alert!</div>
-
-      alert do: "Alert!"
-      #=> <div class="alert">Alert!</div>
-
-  Pass `:only` to generate only block clauses, which is useful for components
-  that nest other components but do not have their own content, like `card`.
-
-      defcomp(:card, block: :only, ...)
-
-      card do: card_header(...)
-      #=> <div class="card">...</div>
-
-      card card_header(...)
-      ** FunctionClauseError ...
-
-  Pass `false` to disable block clause generation.
-
-      defcomp(:card, block: :false, ...)
-
-      card do: card_header(...)
-      ** FunctionClauseError ...
-
-      card card_header(...)
-      #=> <div class="card">...</div>
-
-  ## Prepending Content
-
-  Use the `:prepend` option to prepend other components. This is useful for
-  alerts that can have a close button.
-
-    defcomp(:alert, arity: 3, class: "alert", default_tag: :div, prepend: close_button(role: :alert), variants: [:primary])
+    defcomp(:alert, type: {content_tag, :div}, class: "alert", prepend: close_button(), variants: [:primary])
 
     alert :primary do
       "Content"
@@ -134,27 +91,26 @@ defmodule ExComponent do
           Content
         </div>
 
-  If you pass an atom it will be forwarded to `Phoenix.HTML.Tag/1`. You can pass safe
-  content, like the result of a function call to another component.
+  You can pass an atom that will be forwarded onto `Phoenix.HTML.Tag/1`.
+
+      defcomp(:alert, type: {content_tag, :div}, class: "alert", prepend: :hr, variants: [:primary])
 
   ## Nesting Components
 
-  The `:nest` option allows you to nest the component in the given content. When given a tag name,
-  it is forwarded onto `Phoenix.HTML.Tag.content_tag/3`. You can also use function
-  calls to generate other components.
+  The `:nest` option allows you to nest the component in another tag.
 
   This option is useful for wrapping components in parent tags. For example, breadcrumbs
   in Bootstrap are built with an `ol` tag wrapped in a `nav` tag.
 
-      defcomp(:breadcrumbs, default_tag: :ol, ..., nest: nav())
-      defcomp(:breadcrumbs, default_tag: :ol, ..., nest: :nav)
+      defcomp(:breadcrumbs, type: {:content_tag, :ol}, ..., nest: nav())
+      defcomp(:breadcrumbs, type: {:content_tag, :ol}, ..., nest: :nav)
 
   ## Function Delegation
 
   Pass a function capture to the `:delegate` option to forward execution to
   another module. This is useful for generating tags using `Phoenix.HTML`.
 
-      defcomp(:card_image, class: "card-img", delegate: &Phoenix.HTML.Tag.img_tag/2)
+      defcomp(:card_image, type: {:delegate, &Phoenix.HTML.Tag.img_tag/2}, class: "card-img")
 
       card_image "path", class: "extra"
       #=> <img src="path" class="card-image extra">
@@ -164,58 +120,74 @@ defmodule ExComponent do
   All `:html_opts` are forwarded onto the underlying HTML. Any keys can be
   overriden during function calls.
 
-      defcomp(:alert, arity: 3, class: "alert", default_tag: :div, html_opts: [role: :alert])
+      defcomp(:alert, type: {:content_tag, :div}, class: "alert", html_opts: [role: :alert])
 
-      #=> <div class="alert" role="alert"></div>
+      alert "Alert!"
+      #=> <div class="alert" role="alert">Alert!</div>
 
   """
   import Phoenix.HTML.Tag, only: [tag: 1, tag: 2, content_tag: 3]
 
   defmacro defcomp(name, options) do
-    block = Keyword.get(options, :block, true)
     variants = Keyword.get(options, :variants)
+    type = Keyword.fetch!(options, :type)
 
     quote do
-      if unquote(variants) do
-        def unquote(name)(variant, do: block) when is_atom(variant) do
-          unquote(name)([variants: variant], do: block)
-        end
+      case unquote(type) do
+        {type, name} when type in [:content_tag, :delegate] ->
+          if unquote(variants) do
+            def unquote(name)(variant, do: block) when is_atom(variant) do
+              unquote(name)([variants: variant], do: block)
+            end
 
-        def unquote(name)(variant, opts, do: block) when is_atom(variant) do
-          unquote(name)([variants: variant] ++ opts, do: block)
-        end
+            def unquote(name)(variant, opts, do: block) when is_atom(variant) do
+              unquote(name)([variants: variant] ++ opts, do: block)
+            end
 
-        unless unquote(block) == :only do
-          def unquote(name)(variant, content) when is_binary(content) do
-            unquote(name)([variants: variant], do: content)
+            def unquote(name)(variant, content) when is_binary(content) do
+              unquote(name)([variants: variant], do: content)
+            end
+
+            def unquote(name)(variant, content, opts) when is_binary(content) do
+              unquote(name)([variants: variant] ++ opts, do: content)
+            end
           end
 
-          def unquote(name)(variant, content, opts) when is_binary(content) do
-            unquote(name)([variants: variant] ++ opts, do: content)
+          def unquote(name)(do: block), do: unquote(name)([], do: block)
+
+          def unquote(name)(opts, do: block) do
+            render(unquote(type), opts, unquote(options), do: block)
           end
-        end
-      end
 
-      if unquote(block) do
-        def unquote(name)(do: block), do: unquote(name)([], do: block)
+          def unquote(name)(content), do: unquote(name)(content, [])
 
-        def unquote(name)(opts, do: block) do
-          render(opts, unquote(options), do: block)
-        end
-      end
+          def unquote(name)(content, opts) do
+            render(unquote(type), opts, unquote(options), do: content)
+          end
 
-      unless unquote(block) == :only do
-        def unquote(name)(content), do: unquote(name)(content, [])
+        {:tag, name} ->
+          if unquote(variants) do
+            def unquote(name)(variant) when is_atom(variant) do
+              unquote(name)([variants: variant])
+            end
 
-        def unquote(name)(content, opts) do
-          render(opts, unquote(options), do: content)
-        end
+            def unquote(name)(variant, opts) when is_atom(variant) do
+              unquote(name)([variants: variant] ++ opts)
+            end
+          end
+
+          def unquote(name)(), do: unquote(name)([])
+
+          def unquote(name)(opts) do
+            render(unquote(type), opts, unquote(options))
+          end
+
       end
     end
   end
 
   @doc """
-  Generates HTML content. Accepts a list of options, which is passed
+  Generates a HTML component. Accepts a list of options, which is passed
   onto the underlying HTML.
 
   The `options` argument refers to the component options. See moduledoc
@@ -223,55 +195,59 @@ defmodule ExComponent do
 
   Note that, for variants to work, they must be registered in the component.
 
+  ## Possible Types
+
+    + `{:tag, tag_name}` generates void HTML tags, those that do not accept content. For example, `hr`
+    + `{:content_tag, tag_name}` generates HTML tags that accept their own content
+    + `{:delegate, &function/2}` delegates processing to another function
+
   ## Options
 
-    - `tag` Overrides the `default_tag` in component options
-    - `class` Additional CSS classes to append to the `class` component option
-    - `delegate` Forwards executing to the given function
-    - `variant` An atom or a list of atom component variants
+    - `:tag` Overrides the `default_tag` in component options
+    - `:class` Additional CSS classes to append to the `class` component option
+    - `:variant` An atom or a list of atom component variants
 
   """
-  def render(opts, options, do: block) do
-    element = Keyword.fetch!(options, :render)
-
-    opts =
-      opts
-      |> merge_default_html_opts(options)
-      |> put_class(options)
-      |> drop_options()
-
-    block =
-      options
-      |> Keyword.take([:prepend, :append])
-      |> Keyword.merge(Keyword.take(opts, [:append, :prepend]))
-      |> Enum.reduce([block], fn {sibling, tag_or_content}, acc ->
-        put_sibling(sibling, tag_or_content, acc)
-      end)
-
-    put_content(element, block, opts)
-  end
-
-  defp put_sibling(:prepend, content, acc) do
-    [put_sibling_content(content) | acc]
-  end
-  defp put_sibling(:append, content, acc) do
-    [acc | put_sibling_content(content)]
-  end
-
-  defp put_sibling_content(tag) when is_atom(tag), do: tag(tag)
-  defp put_sibling_content(content), do: content
-
-  defp put_content({:tag, name}, content, opts) do
-    content_tag(name, content, opts)
-  end
-
-  defp put_content({:delegate, fun}, content, opts) do
-    fun.(content, opts)
-  end
-
-  defp put_content({:void, name}, _content, opts) do
+  def render({:tag, name}, opts, options) do
+    opts = put_opts(opts, options)
     tag(name, opts)
   end
+
+  def render({:content_tag, name}, opts, options, do: block) do
+    opts = put_opts(opts, options)
+    block = put_siblings(options, block)
+    content_tag(name, block, opts)
+  end
+
+  def render({:delegate, fun}, opts, options, do: block) do
+    opts = put_opts(opts, options)
+    block = put_siblings(options, block)
+    fun.(block, opts)
+  end
+
+  defp put_opts(opts, options) do
+    opts
+    |> merge_default_html_opts(options)
+    |> put_class(options)
+    |> drop_options()
+  end
+
+  defp put_siblings(options, content) do
+    options
+    |> Keyword.take([:prepend, :append])
+    |> Enum.reduce([content], fn {sibling, tag_or_content}, acc ->
+      case sibling do
+        :prepend ->
+          [put_sibling(tag_or_content) | acc]
+
+        :append ->
+          [acc | put_sibling(tag_or_content)]
+      end
+    end)
+  end
+
+  defp put_sibling(tag) when is_atom(tag), do: tag(tag)
+  defp put_sibling(content), do: content
 
   defp put_class(opts, options) do
     base_class = Keyword.fetch!(options, :class)
--- test/ex_component_test.exs
+++ test/ex_component_test.exs
@@ -4,16 +4,16 @@ defmodule ExComponentTest do
   import ExComponent
   import Phoenix.HTML, only: [safe_to_string: 1]
 
-  describe "defcomp with `arity: 2`" do
-    defmodule Default do
-      defcomp(:list, render: {:tag, :ul}, class: "list")
+  describe "defcomp with content_tag" do
+    defmodule List do
+      defcomp(:list, type: {:content_tag, :ul}, class: "list", variants: [:flush, :horizontal])
     end
 
     test "generates component with block" do
       expected = ~s(<ul class=\"list\">Content</ul>)
 
       result =
-        Default.list do
+        List.list do
           "Content"
         end
 
@@ -24,7 +24,7 @@ defmodule ExComponentTest do
       expected = ~s(<ul class=\"list extra\">Content</ul>)
 
       result =
-        Default.list class: "extra" do
+        List.list class: "extra" do
           "Content"
         end
 
@@ -34,7 +34,7 @@ defmodule ExComponentTest do
     test "generates component with content" do
       expected = ~s(<ul class=\"list\">Content</ul>)
 
-      result = Default.list("Content")
+      result = List.list("Content")
 
       assert safe_to_string(result) == expected
     end
@@ -42,100 +42,16 @@ defmodule ExComponentTest do
     test "generates component with content and opts" do
       expected = ~s(<ul class=\"list extra\">Content</ul>)
 
-      result = Default.list("Content", class: "extra")
+      result = List.list("Content", class: "extra")
 
       assert safe_to_string(result) == expected
-    end
-  end
-
-  describe "defcomp with `arity: 2` and block false" do
-    defmodule NoBlock do
-      defcomp(:list, render: {:tag, :ul}, block: false, class: "list")
-    end
-
-    test "generates component with content" do
-      expected = ~s(<ul class=\"list\">Content</ul>)
-
-      result = NoBlock.list("Content")
-
-      assert safe_to_string(result) == expected
-    end
-
-    test "generates component with content and opts" do
-      expected = ~s(<ul class=\"list extra\">Content</ul>)
-
-      result = NoBlock.list("Content", class: "extra")
-
-      assert safe_to_string(result) == expected
-    end
-
-    test "raises when given a block" do
-      assert_raise ArgumentError, fn ->
-        NoBlock.list do
-          "Content"
-        end
-      end
-    end
-
-    test "raises when given a block and opts" do
-      assert_raise ArgumentError, fn ->
-        NoBlock.list class: "extra" do
-          "Content"
-        end
-      end
-    end
-  end
-
-  describe "defcomp with `arity: 2` and `block: :only`" do
-    defmodule BlockOnly do
-      defcomp(:list, render: {:tag, :ul}, block: :only, class: "list")
-    end
-
-    test "generates component with a block" do
-      expected = ~s(<ul class=\"list\">Content</ul>)
-
-      result =
-        BlockOnly.list do
-          "Content"
-        end
-
-      assert safe_to_string(result) == expected
-    end
-
-    test "generates component with a block and opts" do
-      expected = ~s(<ul class=\"list extra\">Content</ul>)
-
-      result =
-        BlockOnly.list class: "extra" do
-          "Content"
-        end
-
-      assert safe_to_string(result) == expected
-    end
-
-    test "raises when given content" do
-      assert_raise FunctionClauseError, fn ->
-        BlockOnly.list("Content")
-      end
-    end
-
-    test "raises when given content and opts" do
-      assert_raise FunctionClauseError, fn ->
-        BlockOnly.list("Content", class: "extra")
-      end
-    end
-  end
-
-  describe "defcomp with `arity: 3`" do
-    defmodule ArityThreeDefault do
-      defcomp(:list, render: {:tag, :ul}, class: "list", variants: [:flush, :horizontal])
     end
 
     test "generates component with atom variant and block" do
       expected = ~s(<ul class=\"list list-flush\">Content</ul>)
 
       result =
-        ArityThreeDefault.list :flush do
+        List.list :flush do
           "Content"
         end
 
@@ -146,7 +62,7 @@ defmodule ExComponentTest do
       expected = ~s(<ul class=\"list list-flush extra\">Content</ul>)
 
       result =
-        ArityThreeDefault.list :flush, class: "extra" do
+        List.list :flush, class: "extra" do
           "Content"
         end
 
@@ -156,7 +72,7 @@ defmodule ExComponentTest do
     test "generates component with atom variant and content" do
       expected = ~s(<ul class=\"list list-flush\">Content</ul>)
 
-      result = ArityThreeDefault.list(:flush, "Content")
+      result = List.list(:flush, "Content")
 
       assert safe_to_string(result) == expected
     end
@@ -164,78 +80,54 @@ defmodule ExComponentTest do
     test "generates component with atom variant, content and opts" do
       expected = ~s(<ul class=\"list list-flush extra\">Content</ul>)
 
-      result = ArityThreeDefault.list(:flush, "Content", class: "extra")
+      result = List.list(:flush, "Content", class: "extra")
 
       assert safe_to_string(result) == expected
     end
   end
 
-  describe "defcomp with `arity: 3` with `block: only`" do
-    defmodule ArityThreeBlockOnly do
-      defcomp(:list,
-        render: {:tag, :ul},
-        block: :only,
-        class: "list",
-        variants: [:flush, :horizontal]
-      )
+  describe "defcomp with tag" do
+    defmodule Divider do
+      defcomp(:divider, type: {:tag, :hr}, class: "divider", variants: [:sm, :lg])
     end
 
-    test "generates component with atom variant and block" do
-      expected = ~s(<ul class=\"list list-flush\">Content</ul>)
+    test "generates component" do
+      expected = ~s(<hr class=\"divider\">)
 
-      result =
-        ArityThreeBlockOnly.list :flush do
-          "Content"
-        end
+      result = Divider.divider
 
       assert safe_to_string(result) == expected
     end
 
-    test "generates component with atom variant, opts and block" do
-      expected = ~s(<ul class=\"list list-flush extra\">Content</ul>)
+    test "generates component with opts" do
+      expected = ~s(<hr class=\"divider extra\">)
 
-      result =
-        ArityThreeBlockOnly.list :flush, class: "extra" do
-          "Content"
-        end
+      result = Divider.divider class: "extra"
 
       assert safe_to_string(result) == expected
     end
 
-    test "raises when given content" do
-      assert_raise FunctionClauseError, fn ->
-        ArityThreeBlockOnly.list(:flush, "Content")
-      end
+    test "generates component with atom variant" do
+      expected = ~s(<hr class=\"divider divider-sm\">)
+
+      result = Divider.divider :sm
+
+      assert safe_to_string(result) == expected
     end
 
-    test "raises when given content and opts" do
-      assert_raise FunctionClauseError, fn ->
-        ArityThreeBlockOnly.list("Content", class: "extra")
-      end
-    end
-  end
+    test "generates component with atom variant and opts" do
+      expected = ~s(<hr class=\"divider divider-sm extra\">)
 
-  describe "with tag option" do
-    defmodule F do
-      defcomp(:list, render: {:tag, :ul}, class: "list")
-    end
-
-    test "generates component with a custom tag" do
-      expected = ~s(<div class=\"list\">Content</div>)
-
-      result =
-        F.list tag: :div do
-          "Content"
-        end
+      result = Divider.divider :sm, class: "extra"
 
       assert safe_to_string(result) == expected
     end
   end
 
-  describe "with component delegate option" do
+  describe "defcomp with :delegate" do
     defmodule Delegate do
       alias Phoenix.HTML.Tag
-      defcomp(:image, render: {:delegate, &Tag.img_tag/2}, class: "image")
+      defcomp(:image, type: {:delegate, &Tag.img_tag/2}, class: "image")
     end
 
     test "delegates to the given function" do
@@ -246,6 +138,24 @@ defmodule ExComponentTest do
       assert safe_to_string(result) == expected
     end
   end
+
+  # describe "with tag option" do
+  #   defmodule F do
+  #     defcomp(:list, render: {:tag, :ul}, class: "list")
+  #   end
+
+  #   test "generates component with a custom tag" do
+  #     expected = ~s(<div class=\"list\">Content</div>)
+
+  #     result =
+  #       F.list tag: :div do
+  #         "Content"
+  #       end
+
+  #     assert safe_to_string(result) == expected
+  #   end
+  # end
+
 
   # describe "with function delegate option" do
   #   alias Phoenix.HTML.{Tag, Link}
@@ -262,33 +172,33 @@ defmodule ExComponentTest do
   #   end
   # end
 
-  describe "with variants option" do
-    defmodule I do
-      defcomp(:list, render: {:tag, :ul}, class: "list", variants: [:flush, :horizontal])
-    end
+  # describe "with variants option" do
+  #   defmodule I do
+  #     defcomp(:list, render: {:tag, :ul}, class: "list", variants: [:flush, :horizontal])
+  #   end
 
-    test "generates component with an atom variant option" do
-      expected = ~s(<ul class=\"list list-flush\">Content</ul>)
+  #   test "generates component with an atom variant option" do
+  #     expected = ~s(<ul class=\"list list-flush\">Content</ul>)
 
-      result =
-        I.list variants: :flush do
-          "Content"
-        end
+  #     result =
+  #       I.list variants: :flush do
+  #         "Content"
+  #       end
 
-      assert safe_to_string(result) == expected
-    end
+  #     assert safe_to_string(result) == expected
+  #   end
 
-    test "generates component with a list variant option" do
-      expected = ~s(<ul class=\"list list-flush list-horizontal\">Content</ul>)
+  #   test "generates component with a list variant option" do
+  #     expected = ~s(<ul class=\"list list-flush list-horizontal\">Content</ul>)
 
-      result =
-        I.list variants: [:flush, :horizontal] do
-          "Content"
-        end
+  #     result =
+  #       I.list variants: [:flush, :horizontal] do
+  #         "Content"
+  #       end
 
-      assert safe_to_string(result) == expected
-    end
-  end
+  #     assert safe_to_string(result) == expected
+  #   end
+  # end
 
   describe "with prepend option" do
     # defmodule Icon do
@@ -310,20 +220,20 @@ defmodule ExComponentTest do
     #   assert safe_to_string(result) == expected
     # end
 
-    defmodule PrependTag do
-      defcomp(:breadcrumb, render: {:tag, :ol}, class: "breadcrumb", prepend: {:void, :hr})
-    end
+    # defmodule PrependTag do
+    #   defcomp(:breadcrumb, type: {:content_tag, :ol}, class: "breadcrumb", prepend: :hr)
+    # end
 
-    test "prepends the given tag to the component" do
-      expected = ~s(<ol class=\"breadcrumb\"><hr>Content</ol>)
+    # test "prepends the given tag to the component" do
+    #   expected = ~s(<ol class=\"breadcrumb\"><hr>Content</ol>)
 
-      result =
-        PrependTag.breadcrumb do
-          "Content"
-        end
+    #   result =
+    #     PrependTag.breadcrumb do
+    #       "Content"
+    #     end
 
-      assert safe_to_string(result) == expected
-    end
+    #   assert safe_to_string(result) == expected
+    # end
   end
 
   # describe "with `nest` option" do
